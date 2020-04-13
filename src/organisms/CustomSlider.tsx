6d8756254aa12fb0a3c3bde46480e1ff21034d5b
amdigbari <amirmahdidigbari@yahoo.com>
2020-04-13 15:49:56 +0430

    index page added. better to have better styles

---
+++ src/organisms/CustomSlider.tsx
@@ -0,0 +1,39 @@
+import { ReactElement, useState, FC, useCallback } from 'react';
+import ItemsCarousel from 'react-items-carousel';
+
+export type customSliderProps<T> = {
+    items: Array<T>;
+    renderItem: FC<{ item: T }>;
+};
+
+function CustomSlider<T>({ items, renderItem: RenderItem }: customSliderProps<T>): ReactElement {
+    const [activeItemIndex, setActiveItemIndex] = useState<number>(0);
+
+    const RenderButton: FC<{ text: string }> = useCallback(
+        ({ text }) => (
+            <button className="w-8 h-8 rounded-full bg-tertiary text-gray-dark focus:outline-none transition-colors ease-linear duration-150 focus:bg-gray-lightest">
+                {text}
+            </button>
+        ),
+        [],
+    );
+
+    return (
+        <div style={{ padding: `0 40px` }}>
+            <ItemsCarousel
+                requestToChangeActive={setActiveItemIndex}
+                activeItemIndex={activeItemIndex}
+                numberOfCards={4}
+                gutter={20}
+                leftChevron={<RenderButton text="<" />}
+                rightChevron={<RenderButton text=">" />}
+                outsideChevron
+                chevronWidth={40}>
+                {items.map((item, index) => (
+                    <RenderItem item={item} key={index} />
+                ))}
+            </ItemsCarousel>
+        </div>
+    );
+}
+export default CustomSlider;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
 import { NextPage } from 'next';
+import { FC, useCallback } from 'react';
 
 import NavLink from 'atoms/NavLink';
 import Banner from 'atoms/Banner';
+import CustomSlider, { customSliderProps } from 'organisms/CustomSlider';
+
+type itemType = { name: string };
+
+const items: Array<itemType> = [
+    { name: 'First Item' },
+    { name: 'Second Item' },
+    { name: 'Third Item' },
+    { name: 'Fourth Item' },
+    { name: 'Fifth Item' },
+    { name: 'Sixth Item' },
+    { name: 'Seventh Item' },
+    { name: 'Eighth Item' },
+    { name: 'Ninth Item' },
+    { name: 'Tenth Item' },
+];
 
 const Home: NextPage<{}> = () => {
+    const RenderItem: customSliderProps<itemType>['renderItem'] = useCallback(({ item }) => {
+        return <div className="bg-gray-light h-20 flex align-center justify-center">{item.name}</div>;
+    }, []);
+
     return (
         <div className="flex flex-col container items-center p-3">
             <div className="w-full flex justify-end">
@@ -12,6 +33,21 @@ const Home: NextPage<{}> = () => {
 
             <div className="w-5/6 mt-8">
                 <Banner src="/images/homepage/design_banner.jpg" href="/design/" />
+            </div>
+
+            <div className="w-full w-5/6 mt-12" dir="ltr">
+                <h5 className="text-short text-right mb-3" dir="rtl">
+                    تخفیف‌دارها:
+                </h5>
+
+                <CustomSlider<itemType> items={items} renderItem={RenderItem} />
+            </div>
+            <div className="w-full w-5/6 mt-12" dir="ltr">
+                <h5 className="text-short text-right mb-3" dir="rtl">
+                    جدیدها:
+                </h5>
+
+                <CustomSlider<itemType> items={items} renderItem={RenderItem} />
             </div>
         </div>
     );
